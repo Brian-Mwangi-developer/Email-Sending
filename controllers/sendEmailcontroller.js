d0a1a093676802109892581a37bce1c38615a6c0
Brian Mwangi <britabyzk29@gmail.com>
2023-11-02 10:45:27 +0300

    send data to database

--- controllers/sendEmailcontroller.js
+++ controllers/sendEmailcontroller.js
@@ -1,30 +1,39 @@
-
+const mysql = require("mysql");
 const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth:{
+    auth: {
         user: process.env.EMAIL_ADDRESS,
         pass: process.env.EMAIL_PASSWORD,
     },
 });
 
-const sendEmail = (req, res)=>{
+const pool = mysql.createPool({
+    connectionLimit:10,
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'Creditors'
+})
+
+const sendEmail = (req, res) => {
     console.log("sending....")
-    try{
-        const  { name, email,phoneNumber,nationalId,productId, productName}= req.body;
+    try {
+        const { name, email, phoneNumber, nationalId, productId, productName } = req.body;
         const { files, idPhoto } = req.files;
         const pdfAttachments = files.filter((file) => file.mimetype === 'application/pdf');
         const imageAttachments = [idPhoto].concat(files.filter((file) => file.mimetype === 'image/jpeg'));
         console.log(req.body)
         console.log(files)
-        
+        console.log(idPhoto)
+
         let mailOptions = {
-            from        : process.env.EMAIL_ADDRESS,
-            to          : "exampleme999@gmail.com",
-            subject     : `Application for${nationalId}`,
-            html        : ` 
+            from: process.env.EMAIL_ADDRESS,
+            to: "exampleme999@gmail.com",
+            subject: `Application for${nationalId}`,
+            html: ` 
                             <div>
                                 <p>Name: ${name}</p>
                                 <p>NationalId: ${nationalId}</p>
@@ -34,28 +43,62 @@ const sendEmail = (req, res)=>{
                                 <p>Phone Number: ${phoneNumber}</p>
                             </div>
                             `,
-                            attachments: [...pdfAttachments, ...imageAttachments].concat(idPhoto),
+            attachments: [...pdfAttachments, ...imageAttachments].concat(idPhoto),
         }
-        transporter.sendMail(mailOptions, (err, data ) => {
-            if(err){
+        transporter.sendMail(mailOptions, (err, data) => {
+            if (err) {
                 res.json({
                     message: err
                 }).status(400)
                 console.log(err)
-            }else{
-                res.json({
-                    status: "success"
-                }).status(200)
-    
-                console.log("Email Sent" + data.response)
+            } else {
+                // If the email is sent successfully, save data to MySQL
+                pool.getConnection((err, connection) => {
+                    if (err) {
+                        res.status(500).json({
+                            message: err,
+                            status: 'database_connection_error'
+                        });
+                        console.log(err);
+                    } else {
+                        const dbData = {
+                            name,
+                            email,
+                            phoneNumber,
+                            nationalId,
+                            productId,
+                            productName,
+                            // Assuming 'files' and 'idPhoto' are file paths
+                            files: files[0].path,
+                            idPhoto: idPhoto[0].path
+                        }
+                        connection.query('INSERT INTO Aspiracredit SET ?', dbData, (err, rows) => {
+                            connection.release();
+                            if (err) {
+                                res.status(500).json({
+                                    message: err,
+                                    status: 'database_insertion_error'
+                                });
+                                console.log(err);
+                            } else {
+                                res.status(200).json({
+                                    message: `Email sent and user with the National id: ${nationalId} has been added`,
+                                    status: 'success'
+                                });
+                                
+                            }
+                        });
+                        console.log('Email Sent: ' + data.response);
+                    }
+                })
             }
-        })
+        });
 
-    }catch(error){
+    } catch (error) {
         res.status(400).send(error)
     }
 
-    
+
 }
 
 module.exports = { sendEmail }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
  const express = require("express");
  const cors = require("cors");
+ const mysql = require("mysql");
  require('dotenv').config();
 
  const detailsRouter = require("./routes/DetailsRoute");
@@ -9,7 +10,7 @@
  const app = express();
  app.use(cors());
  app.use(express.json());
- app.use(express.urlencoded({ extended:true}));
+ app.use(express.urlencoded({ extended:false}));
 
 const PORT =7000;
 
@@ -19,6 +20,69 @@ app.get("/",async(req,res)=>{
 
 app.use("/api/", detailsRouter);
 app.use("/api",emailRouter)
+
+//mysql section
+const pool = mysql.createPool({
+    connectionLimit:10,
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'Creditors'
+})
+
+app.get('/api/mysql',(req,res)=>{
+    pool.getConnection((err, connection)=>{
+        if(err) throw err;
+        console.log(`connection in id ${connection.threadId}`);
+        connection.query('SELECT * FROM Aspiracredit',(err,rows)=>{
+            connection.release();
+            if(!err){
+                res.send(rows)
+            }else{
+                console.log(err)
+            }
+
+        })
+    })
+})
+
+app.get('/api/mysql/:id',(req,res)=>{
+    pool.getConnection((err, connection)=>{
+        if(err) throw err;
+        console.log(`connection in id ${connection.threadId}`);
+        connection.query('SELECT * FROM Aspiracredit WHERE nationalId = ?',[req.params.id],(err,rows)=>{
+            connection.release();
+
+            if(!err){
+                res.send(rows)
+            }else{
+                console.log(err)
+            }
+            
+        })
+    })
+})
+
+app.post('/api/mysql',(req,res)=>{
+    pool.getConnection((err, connection)=>{
+        if(err) throw err;
+        console.log(`connection in id ${connection.threadId}`);
+        const params = req.body;
+        connection.query('INSERT INTO Aspiracredit SET ?',params,(err,rows)=>{
+            connection.release();
+            if(!err){
+                // Send a success status along with a JSON response
+                res.status(200).json({
+                    status: 'success',
+                    message: `User with the National id: ${params.nationalId} has been added`
+                });
+            }else{
+                console.log(err)
+            }
+            
+        })
+    })
+});
 
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}`);
